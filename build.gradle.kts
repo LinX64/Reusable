569f0df80958ddc12eb1faa291ec8d48efd59b62
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-05-27 19:31:45 +0200

    Update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.mavenPublish) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
